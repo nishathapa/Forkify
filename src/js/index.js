ed33d0166ce27aad89590ba109cd334a7ff571c2
Nisha Thapa <nishathapa4727@gmail.com>
2020-06-26 17:44:14 +0530

    Built search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,5 @@
-import axios from 'axios';
-async function getResults(query) {
-    try{
-        const res = await   axios('https://forkify-api.herokuapp.com/api/search?q=pizza');
-    const recipes = res.data.recipes;
-    console.log(recipes);
-    }
-    catch(error) {
-        alert(error);
-    }
-}
-getResults('pizza');
+import Search from './models/Search';
+
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
