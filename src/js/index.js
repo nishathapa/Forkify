15d6b0199b79845f90b8e4a08130d69936bb7469
Nisha Thapa <nishathapa4727@gmail.com>
2020-06-28 01:18:57 +0530

    Recipe Controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView'
 import { elements, renderLoader, clearLoader } from './views/base';
 
@@ -11,6 +12,8 @@ import { elements, renderLoader, clearLoader } from './views/base';
  */
 
 const state = {};
+
+//SEARCH CONTROLLER
 
 const controlSearch = async () => {
     // 1) GET QUERY FROM VIEW
@@ -26,12 +29,18 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-        // 4) Search for recipes 
+        try{
+             // 4) Search for recipes 
         await state.search.getResults();
 
         // 5) render esults in the UI
         clearLoader();
         searchView.renderResults(state.search.result);
+
+        }catch(err) {
+            alert('Sommething wrong with the search...');
+            clearLoader();
+        }
     }
 }
 
@@ -49,3 +58,42 @@ elements.searchResPages.addEventListener('click', e => {
     }
 })
 
+// RECIPE CONTROLLER
+
+const controlRecipe = async () => {
+
+    //Get Id from URL
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
+
+    if(id) {
+        //Prepare UI for changes
+
+        //Create new recipe object
+        state.recipe = new Recipe(id);
+
+        try {
+             //Get recipe data
+            await state.recipe.getRecipe();
+
+            //Calculate servings and time
+            state.recipe.calcServings();
+            state.recipe.calcTime();
+
+            //Render the recipe
+            console.log(state.recipe);
+        } 
+
+        catch(err) {
+            alert('Error processing recipe');
+        }
+
+       
+    }
+};
+
+/*window.addEventListener('hashchange', controlRecipe);
+window.addEventListener('load', controlRecipe);
+*/
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
