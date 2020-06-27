c99b0fed033faeb286acd9e54c074e5f208147b1
Nisha Thapa <nishathapa4727@gmail.com>
2020-06-28 00:20:23 +0530

    Pagination updated

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -8,6 +8,7 @@ export const clearInput = () => {
 
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
+    elements.searchResPages.innerHTML = '';
 };
 
 /***
@@ -54,10 +55,10 @@ const renderRecipe = recipe => {
 //type = 'prev' or 'next'
 const createButton = (page, type) => `
         <button class="btn-inline results__btn--${type}" data-goto=${type=== 'prev' ? page -1 : page + 1}>
+            <span>Page ${type==='prev' ? page - 1 : page + 1}</span>
             <svg class="search__icon">
                 <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
             </svg>
-            <span>Page ${type==='prev' ? page - 1 : page + 1}</span>
         </button>
 `;
 
