71a2e5c989e49244973ca461029943e4074d1358
Nisha Thapa <nishathapa4727@gmail.com>
2020-06-30 20:37:05 +0530

    Recipe View 2

--- src/js/index.js
+++ src/js/index.js
@@ -76,6 +76,9 @@ const controlRecipe = async () => {
         recipeView.clearRecipe();
         renderLoader(elements.recipe);
 
+        // Highlight the sselected search item
+        if(state.search) searchView.highlightSelected(id);
+
         //Create new recipe object
         state.recipe = new Recipe(id);
 
