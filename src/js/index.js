d5160a234c049e9415115ef4d8fc82bbcdf7d66c
Nisha Thapa <nishathapa4727@gmail.com>
2020-07-01 01:10:24 +0530

    Shopping List model

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 
@@ -85,7 +86,7 @@ const controlRecipe = async () => {
         try {
              //Get recipe data
             await state.recipe.getRecipe();
-            console.log(state.recipe.ingredients);
+          //  console.log(state.recipe.ingredients);
             state.recipe.parseIngredients();
 
             //Calculate servings and time
@@ -129,5 +130,8 @@ elements.recipe.addEventListener('click', e => {
 
     }
 
-    console.log(state.recipe)
-})
+   // console.log(state.recipe)
+});
+
+
+window.l = new List();
